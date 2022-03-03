152f1723c8b027a37a564f02da2bd5aa0921e6e3
Sandu Matei-Cristian <mateicristiansandu21@gmail.com>
2022-03-03 20:04:49 +0200

    2nd week

---
+++ 02_Week2/src/ro/ase/cts/g1099/recap/java/exceptions/InsufficientFundsException.java
@@ -0,0 +1,14 @@
+package ro.ase.cts.g1099.recap.java.exceptions;
+
+public class InsufficientFundsException extends Exception {
+	
+	public InsufficientFundsException(String msg) {
+		super(msg);
+	}
+	
+	public InsufficientFundsException() {
+//		we get it even if we don't write super();
+		super();
+		
+	}
+}
---
+++ 02_Week2/src/ro/ase/cts/g1099/recap/java/models/Profitable.java
@@ -0,0 +1,6 @@
+package ro.ase.cts.g1099.recap.java.models;
+
+public interface Profitable {
+	
+	public abstract void addInterest(double interestPercentage);
+}
