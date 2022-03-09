b6abdc854abf340d0282e60411d379a14e08230e
Sandu Matei-Cristian <mateicristiansandu21@gmail.com>
2022-03-09 09:06:38 +0200

    last commit for today

--- 03_Week3/src/ro/ase/cts/g1099/clean/code/stage3/Product.java
+++ 03_Week3/src/ro/ase/cts/g1099/clean/code/stage3/Product.java
@@ -1,4 +1,4 @@
-package ro.ase.cts.g1099.clean.code.stage2;
+package ro.ase.cts.g1099.clean.code.stage3;
 
 // stage 1
 // naming conventions! ( we need constant values in the source code)
@@ -8,39 +8,28 @@ package ro.ase.cts.g1099.clean.code.stage2;
 // using enums
 // enums are classed in java
 
-// stage 3
-// clean the if-then-else structure
-
 public class Product {
 	public float computeFinalPrice(ProductType productType, float initialPrice, int accountAgeInYears) {
 //		SOURCE -> FORMAT TO REARRANGE CODE
 		final int MAX_ACCOUNT_AGE = 10;
 		final float MAX_ACCOUNT_DISCOUNT = (float) 0.15;
+
+
 		float finalPrice = 0;
 		float accountDiscount = (accountAgeInYears > MAX_ACCOUNT_AGE) ? MAX_ACCOUNT_DISCOUNT
 				: (float) accountAgeInYears / 100;
-
-		switch (productType) {
-		case NEW:
+		if (productType == ProductType.NEW) {
 			finalPrice = initialPrice;
-			break;
-		case DISCOUNT:
+		} else if (productType == ProductType.DISCOUNT) {
 			finalPrice = (initialPrice - (ProductType.DISCOUNT.getDiscount() * initialPrice))
 					- accountDiscount * (initialPrice - (ProductType.DISCOUNT.getDiscount() * initialPrice));
-			break;
-		case SALES:
+		} else if (productType == ProductType.SALES) {
 			finalPrice = (initialPrice - (ProductType.SALES.getDiscount() * initialPrice))
 					- accountDiscount * (initialPrice - (ProductType.SALES.getDiscount() * initialPrice));
-			break;
-		case FINAL_SALES:
+		} else if (productType == ProductType.FINAL_SALES) {
 			finalPrice = (initialPrice - (ProductType.FINAL_SALES.getDiscount() * initialPrice))
 					- accountDiscount * (initialPrice - (ProductType.FINAL_SALES.getDiscount() * initialPrice));
-			break;
-		
-		default:
-			throw new UnsupportedOperationException();
 		}
-
 		return finalPrice;
 	}
 }
