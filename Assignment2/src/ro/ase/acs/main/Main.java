70f84be8d892b53a370d8f1dda54130a51bec7c3
Matei <mateicristiansandu21@gmail.com>
2022-04-04 16:30:53 +0300

    Category: Functions

--- Assignment2/src/ro/ase/acs/main/Main.java
+++ Assignment2/src/ro/ase/acs/main/Main.java
@@ -1,9 +1,26 @@
 package ro.ase.acs.main;
 
+import ro.ase.acs.models.Account;
+import ro.ase.acs.models.AccountType;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		try {
+			Account ac1 = new Account(AccountType.SUPER_PREMIUM, 1.75, 3200);
+			ac1.setDaysActive(399);
+//			System.out.println(ac1.getRateValue());
+			
+			if(ac1.checkForPremiumAccount() == true) 
+				System.out.println("Premium/Super-Premium account");
+
+			
+			System.out.println(ac1);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- Assignment2/src/ro/ase/acs/models/Account.java
+++ Assignment2/src/ro/ase/acs/models/Account.java
@@ -2,40 +2,102 @@ package ro.ase.acs.models;
 
 import ro.ase.acs.interfaces.MonthlyRateInterface;
 
-public class Account implements MonthlyRateInterface{
-	
+public class Account implements MonthlyRateInterface {
+
 	private AccountType accountType;
 	private double loanValue, rateValue;
 	private int daysActive;
+	
+	public Account(AccountType accountType, double loanValue, double rateValue) throws Exception {
+		super();
+		if(this.loanValue < 0  || this.rateValue < 0) {
+			throw new Exception();
+		} else {
+			this.accountType = accountType;
+			this.loanValue = loanValue;
+			this.rateValue = rateValue;
+		}
+	}
+	
+	public Account() {
+		super();
+		this.accountType = AccountType.STANDARD;
+		this.loanValue = 0;
+		this.rateValue = 0;
+		this.daysActive = 0;
+	}
 
 	public double getLoanValue() {
 		System.out.println("The loan value is " + this.loanValue);
 		return loanValue;
 	}
 
+	public void setLoanValue(double loanValue) throws Exception {
+		if (this.loanValue < 0)
+			throw new Exception();
+		else {
+			this.loanValue = loanValue;
+		}
+	}
+
 	public double getRateValue() {
 		System.out.println("The rate is " + rateValue);
 		return this.rateValue;
 	}
-	
+
+	public void setRateValue(double rateValue) throws Exception {
+		if (this.rateValue < 0)
+			throw new Exception();
+		else {
+			this.rateValue = rateValue;
+		}
+	}
+
+	public int getDaysActive() {
+		System.out.println("The number of active days is " + this.daysActive);
+		return daysActive;
+	}
+
+	public void setDaysActive(int daysActive) throws Exception {
+		if(this.daysActive < 0) {
+			throw new Exception();
+		}
+		else {
+			this.daysActive = daysActive;
+		}
+	}
 
 	// must have method - the lead has requested it in all classes
 	@Override
 	public double getMonthlyRate() {
-		
 		return loanValue * rateValue;
 	}
 
-	
-	
-	public void setLoanValue(double value) throws Exception {
-		if (value < 0)
-			throw new Exception();
-		else {
-			loanValue = value;
+//	this function relies on the creation of an account
+	public static double computeTotalFee(Account[] accounts) {
+		double totalFee = 0.0;
+		Account ac;
+		for (int i = 0; i < accounts.length; i++) {
+			ac = accounts[i];
+			if (ac.checkForPremiumAccount() == true)
+				totalFee += .0125 * ac.getTotalValue(ac.loanValue, ac.rateValue, ac.daysActive);
 		}
+		return totalFee;
 	}
 
+	public double getTotalValue(double loanValue, double rateValue, int daysActive) {
+		return loanValue * Math.pow(rateValue, (daysActive / 365)) - loanValue;
+	}
+
+//	function for checking whether an account is premium or not
+	public boolean checkForPremiumAccount() {
+		if (accountType == AccountType.PREMIUM || accountType == AccountType.SUPER_PREMIUM) {
+			return true;
+		} else
+			return false;
+	}
+
+//	StringBuider/StringBuffer
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -50,31 +112,4 @@ public class Account implements MonthlyRateInterface{
 		builder.append("]");
 		return builder.toString();
 	}
-
-//	this function relies on the creation of an account
-	public static double computeTotalFee(Account[] accounts) {
-		double totalFee = 0.0;
-		Account instance;
-		for (int i = 0; i < accounts.length; i++) {
-			instance = accounts[i];
-			if (instance.accountType == AccountType.PREMIUM || instance.accountType == AccountType.SUPER_PREMIUM)
-				totalFee += .0125 * ( // 1.25% broker's fee
-				instance.loanValue * Math.pow(instance.rateValue, (instance.daysActive / 365)) - instance.loanValue); // interest-principal
-		}
-		return totalFee;
-	}
-
-//	constructor
-	public Account(double value, double rate, AccountType account_Type) throws Exception {
-		if (value < 0)
-			throw new Exception();
-		else {
-			loanValue = value;
-		}
-		this.rateValue = rate;
-		this.accountType = account_Type;
-	}
-
-
-
 }
